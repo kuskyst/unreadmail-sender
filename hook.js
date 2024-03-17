ba4995147efd2a13fcb25fd16ebea1095db3d522
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-03-17 21:00:54 +0900

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,50 +1,40 @@
 function hook() {
- const threads = GmailApp.search('label:unread');  // 未読のスレッドを取得
+ const threads = GmailApp.search('label:unread');
 
  if (threads.length == 0) {
-   Logger.log('新規メッセージなし');
+   Logger.log('no unread mail !');
    return
  }
 
  threads.forEach(function (thread) {
-   const messages = thread.getMessages();
+    const messages = thread.getMessages();
 
-   const payloads = messages.map(function (message) {
-     message.markRead();
+    const payloads = messages.map(function (message) {
+      message.markRead();
 
-     const from = message.getFrom();
-     const subject = message.getSubject();
-     const plainBody = message.getPlainBody();
+      const from = message.getFrom();
+      const subject = message.getSubject();
+      const plainBody = message.getPlainBody();
+      const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+      const sheet = spreadsheet.getActiveSheet();
+      const webhook = sheet.getRange(1, 1).getValue();
 
-     const webhook = getWebhookUrl();
-
-     Logger.log(subject);
-     const payload = {
-       content: subject,
-       embeds: [{
-         title: subject,
-         author: {
-           name: from,
-         },
-         description: plainBody.substr(0, 2048),
+      Logger.log(subject);
+      const payload = {
+        content: subject,
+        embeds: [{
+          title: subject,
+          author: { name: from },
+          description: plainBody.substr(0, 2048),
        }],
-     }
-     return {
-       url: webhook,
-       contentType: 'application/json',
-       payload: JSON.stringify(payload),
-     }
-   })
-
-   Logger.log(payloads);
-   UrlFetchApp.fetchAll(payloads);
- })
-}
-
-
-function getWebhookUrl() {
- const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
- const sheet = spreadsheet.getActiveSheet();
-
- return sheet.getRange(1, 1).getValue();
+      }
+      return {
+        url: webhook,
+        contentType: 'application/json',
+        payload: JSON.stringify(payload),
+      }
+    })
+    Logger.log(payloads);
+    UrlFetchApp.fetchAll(payloads);
+  })
 }
